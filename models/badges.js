f62458b8617c827290ab308f0dc872ab155d6953
Elvio Cavalcante <elvio@madfx.com.br>
2015-03-13 02:49:11 -0300

    Lib directory created

--- models/badges.js
+++ models/badges.js
@@ -1,1 +1,14 @@
 'use strict';
+
+var redis = require('../lib/redis');
+
+/**
+ * Save badges to database
+ * @param {Array} badges
+ * @param {Function} callback 
+
+*/
+
+exports.save = function(badges, callback){
+
+};
