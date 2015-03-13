b5c21685528e2c97ebd2786d97b77ed4dc1ffc38
Elvio Cavalcante <elvio@madfx.com.br>
2015-03-13 02:40:46 -0300

    Minor changes to controllers. Models folder created

--- controllers/badges.js
+++ controllers/badges.js
@@ -1,3 +1,16 @@
 'use strict';
 
-exports.save = function(){};
+var _ = require('underscore');
+var model = require('../models/badges');
+
+// Send badges to model to be saved
+
+exports.save = function(req, res, next){
+  // basically, when "next" is involked it says: "I'm done with this controller, move to the next one."
+	// For security reasons, we will clone the req.body to prevent inusited changes into the req.body object
+	var badges = _.clone(req.body);
+};
+
+// Send badges to pub/sub socket in model
+
+exports.send = function(){};
